2e296b3d6ad61addf103c0f2b3f6d1f221397beb
Fernanda <fernandasousacampos6@gmail.com>
2025-04-16 09:52:41 -0300

    bingo_marcia.py

--- marcia/bingo_marcia.py
+++ marcia/bingo_marcia.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-import time
 
 pygame.init()
 largura, altura = 550, 600
@@ -10,13 +9,13 @@ pygame.display.set_caption("Bingo da mae marcia")
 fonte = pygame.font.SysFont("arial", 25)
 fonte_bingo = pygame.font.SysFont("arial", 40, bold=True)
 
-# cor
+# cores
 roxo = (150, 0, 200)
 branco = (255, 255, 255)
 preto = (0, 0, 0)
-rosa = (147,112,219)
+rosa = (147, 112, 219)
 
-
+# cartelinha cm numeros
 cartela = []
 colunas = {
     "B": random.sample(range(1, 16), 5),
@@ -30,13 +29,13 @@ for i in range(5):
     linha = [colunas["B"][i], colunas["I"][i], colunas["N"][i], colunas["G"][i], colunas["O"][i]]
     cartela.append(linha)
 
-cartela[2][2] = "❤️"  
-# arrumar 
+cartela[2][2] = "❤️"  # dxa o centro sem nd (hello kitty)
+
+# imagem da hello kitty no meio
 hello = pygame.image.load("hellokitty.png")
 hello = pygame.transform.scale(hello, (78, 65))
 
-sorteados = []
-
+# looop
 rodando = True
 while rodando:
     tela.fill(rosa)
@@ -47,7 +46,7 @@ while rodando:
         texto = fonte_bingo.render(letra, True, roxo)
         tela.blit(texto, (i * 100 + 30, 20))
 
-    # por favor funciona porra 
+    # por favor funciona porraaaaaaaaaa
     for y in range(5):
         for x in range(5):
             num = cartela[y][x]
@@ -56,10 +55,9 @@ while rodando:
             pygame.draw.rect(tela, roxo, rect, 3)
 
             if num == "❤️":
-                tela.blit(hello, (x * 100 + 20, y * 100 + 70))
+                tela.blit(hello, (x * 100 + 20, y * 100 + 70))  # meio sem nada
             else:
-                cor = roxo if num in sorteados else preto
-                texto = fonte.render(str(num), True, cor)
+                texto = fonte.render(str(num), True, preto)  #exibe os numeros na cor preta 
                 tela.blit(texto, (x * 100 + 45, y * 100 + 100))
 
     pygame.display.flip()
